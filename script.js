049bbe4028fbb0346408dbfd91d2dccc758b6870
TEXTIL\lucas.matias <lucas.matias@tbmtextil.com.br>
2026-01-03 09:34:22 -0300

    dia tres

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const PRODUCTS = [
   { id: "kit12", nome: "Kit com 24 unidades", preco: 74.00, imagem: "2.jpg" },
   { id: "Pudim", nome: "Pudim Tradicional", preco: 6.0, imagem: "pudim.jpg.avif" },
   { id: "Bolo", nome: "Bolo Tradicional", preco: 8.0, imagem: "bolo.jpg" },
-  
+    
   ];
 
 let currentSale = [];
